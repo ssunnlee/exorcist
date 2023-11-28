8cf7673f98fe068cc196e7ce17f5089fbb57ec18
Sunho Lee <sunhol1@uci.edu>
2023-11-28 15:41:51 -0800

    Turned PPO agent into class

--- PPO.py
+++ PPO.py
@@ -9,25 +9,6 @@ from tqdm import tqdm
 import gym
 # import gym_pygame
 
-env_id = "ALE/DemonAttack-v5"
-env = gym.make(env_id, obs_type="grayscale")
-eval_env = gym.make(env_id, obs_type="grayscale")
-s_size = env.observation_space.shape
-a_size = env.action_space.n
-
-HEIGHT = s_size[0]
-WIDTH = s_size[1]
-#CHANNEL_SIZE = s_size[2]
-DIM = HEIGHT * WIDTH
-
-
-# Hyperparameters
-LEARNING_RATE = 1e-4
-GAMMA = 0.99
-EPOCHS = 10
-CLIP_EPSILON = 0.2
-BATCH_SIZE = 64
-
 """
 # Sample environment with discrete actions (replace with your environment)
 class SampleEnv:
@@ -67,75 +48,107 @@ class ValueNetwork(nn.Module):
 
     def forward(self, state):
         return self.model(state)
+    
 
-policy_net = PolicyNetwork(DIM, 16, 6)
-value_net = ValueNetwork(4, 16)
-policy_optimizer = optim.Adam(policy_net.parameters(), lr=LEARNING_RATE)
-value_optimizer = optim.Adam(value_net.parameters(), lr=LEARNING_RATE)
-criterion = nn.MSELoss()
+class PPO_agent:
+    def __init__(self, dimensions, learning_rate, gamma, epochs, epsilon, batch_size):
+        self.dimensions = dimensions
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.epochs = epochs
+        self.epsilon = epsilon
+        self.batch_size = batch_size
 
-def compute_returns(rewards):
-    returns = []
-    R = 0
-    for r in reversed(rewards):
-        R = r + GAMMA * R
-        returns.insert(0, R)
-    return torch.tensor(returns)
+        self.policy_net = PolicyNetwork(DIM, 16, 6)
+        self.value_net = ValueNetwork(4, 16)
+        self.policy_optimizer = optim.Adam(self.policy_net.parameters(), lr=LEARNING_RATE)
+        self.value_optimizer = optim.Adam(self.value_net.parameters(), lr=LEARNING_RATE)
+        self.criterion = nn.MSELoss()
 
-def ppo_step():
-    state = env.reset()[0]
-    done = False
-    states, actions, log_probs_old, rewards = [], [], [], []
+    def compute_returns(self, rewards):
+        returns = []
+        R = 0
+        for r in reversed(rewards):
+            R = r + GAMMA * R
+            returns.insert(0, R)
+        return torch.tensor(returns)
 
-    counter = 0
-    while not done:
-        action_probs = policy_net(state)
-        action = torch.multinomial(action_probs, 1).item()
-        #print("\n", action)
-        #print(env.step(action))
-        next_state, reward, done, _truncated, info = env.step(action)
+    def ppo_step(self):
+        state = env.reset()[0]
+        done = False
+        states, actions, log_probs_old, rewards = [], [], [], []
 
-        states.append(state)
-        actions.append(action)
-        log_probs_old.append(torch.log(action_probs[0, action]))
-        rewards.append(reward)
+        counter = 0
+        while not done:
+            action_probs = self.policy_net(state)
+            action = torch.multinomial(action_probs, 1).item()
+            #print("\n", action)
+            #print(env.step(action))
+            next_state, reward, done, _truncated, info = env.step(action)
 
-        state = next_state
-        print(counter)
-        if counter == 100000:
-            done = True
-        counter += 1
+            states.append(state)
+            actions.append(action)
+            log_probs_old.append(torch.log(action_probs[0, action]))
+            rewards.append(reward)
 
-    returns = compute_returns(rewards)
-    values = value_net(torch.stack(states))
-    advantages = returns - values.squeeze()
+            state = next_state
+            print(counter)
+            if counter == 100000:
+                done = True
+            counter += 1
 
-    for _ in range(EPOCHS):
-        for i in range(0, len(states), BATCH_SIZE):
-            batch_states = torch.stack(states[i:i+BATCH_SIZE])
-            batch_actions = torch.tensor(actions[i:i+BATCH_SIZE])
-            batch_log_probs_old = torch.stack(log_probs_old[i:i+BATCH_SIZE])
-            batch_advantages = advantages[i:i+BATCH_SIZE]
-            batch_returns = returns[i:i+BATCH_SIZE]
+        returns = self.compute_returns(rewards)
+        values = self.value_net(torch.stack(states))
+        advantages = returns - values.squeeze()
 
-            new_action_probs = policy_net(batch_states)
-            new_log_probs = torch.log(new_action_probs.gather(1, batch_actions.unsqueeze(-1)))
-            ratio = (new_log_probs - batch_log_probs_old).exp()
+        for _ in range(EPOCHS):
+            for i in range(0, len(states), BATCH_SIZE):
+                batch_states = torch.stack(states[i:i+BATCH_SIZE])
+                batch_actions = torch.tensor(actions[i:i+BATCH_SIZE])
+                batch_log_probs_old = torch.stack(log_probs_old[i:i+BATCH_SIZE])
+                batch_advantages = advantages[i:i+BATCH_SIZE]
+                batch_returns = returns[i:i+BATCH_SIZE]
 
-            surrogate_obj1 = ratio * batch_advantages
-            surrogate_obj2 = torch.clamp(ratio, 1-CLIP_EPSILON, 1+CLIP_EPSILON) * batch_advantages
-            policy_loss = -torch.min(surrogate_obj1, surrogate_obj2).mean()
+                new_action_probs = self.policy_net(batch_states)
+                new_log_probs = torch.log(new_action_probs.gather(1, batch_actions.unsqueeze(-1)))
+                ratio = (new_log_probs - batch_log_probs_old).exp()
 
-            value_loss = criterion(value_net(batch_states), batch_returns.unsqueeze(-1))
+                surrogate_obj1 = ratio * batch_advantages
+                surrogate_obj2 = torch.clamp(ratio, 1-CLIP_EPSILON, 1+CLIP_EPSILON) * batch_advantages
+                policy_loss = -torch.min(surrogate_obj1, surrogate_obj2).mean()
 
-            policy_optimizer.zero_grad()
-            policy_loss.backward()
-            policy_optimizer.step()
+                value_loss = self.criterion(self.value_net(batch_states), batch_returns.unsqueeze(-1))
 
-            value_optimizer.zero_grad()
-            value_loss.backward()
-            value_optimizer.step()
+                self.policy_optimizer.zero_grad()
+                policy_loss.backward()
+                self.policy_optimizer.step()
+
+                self.value_optimizer.zero_grad()
+                value_loss.backward()
+                self.value_optimizer.step()
 
 # Training loop
-for _ in tqdm(range(1000)):
-    ppo_step()
+if __name__ == "__main__":
+    env_id = "ALE/DemonAttack-v5"
+    env = gym.make(env_id, obs_type="grayscale")
+    eval_env = gym.make(env_id, obs_type="grayscale")
+    s_size = env.observation_space.shape
+    a_size = env.action_space.n
+
+    HEIGHT = s_size[0]
+    WIDTH = s_size[1]
+    #CHANNEL_SIZE = s_size[2]
+    DIM = HEIGHT * WIDTH
+
+
+    # Hyperparameters
+    LEARNING_RATE = 1e-4
+    GAMMA = 0.99
+    EPOCHS = 10
+    CLIP_EPSILON = 0.2
+    BATCH_SIZE = 64
+
+    agent = PPO_agent(DIM, LEARNING_RATE, GAMMA, EPOCHS, CLIP_EPSILON, BATCH_SIZE)
+
+    for _ in tqdm(range(1000)):
+        agent.ppo_step()
